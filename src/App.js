d9cd3abc8cb8a779ebeea6052a17a52f9539cbeb
etrigo <bjorn.sjostedt@etrigo.com>
2022-04-24 10:59:39 +0200

    Overall fixes

--- src/App.js
+++ src/App.js
@@ -14,98 +14,101 @@ const App = () => {
   const [start, setStart] = useState(false)
   const [intervalId, setIntervalId] = useState(0)
   const [sessionTimer, setSessionTimer] = useState(true)
+  const [timerTitle, setTimerTitle] = useState('Session')
   const [millie, setMillie] = useState(convertToMillie(sessionLength))
-  // const [timer, setTimer] = useState(convertToTimer())
 
+  // use of hook for audio
   const audio = useRef(null)
 
-  // listen to change of timer, session or break timer or when program switch timer
+  // listen to change of timer type and timer title
   useEffect(() => {
     sessionTimer
       ? setMillie(convertToMillie(sessionLength))
       : setMillie(convertToMillie(breakLength))
-  }, [sessionTimer, sessionLength, breakLength])
 
-  // set timer when user changes break or session
+    sessionTimer ? setTimerTitle('Session') : setTimerTitle('Break')
+  }, [sessionTimer])
+
+  // Listen to change in session timer length
+  useEffect(() => {
+    sessionTimer && setMillie(convertToMillie(sessionLength))
+  }, [sessionLength])
+
+  // listen to change in break length
+  useEffect(() => {
+    !sessionTimer && setMillie(convertToMillie(breakLength))
+  }, [breakLength])
+
+  // set timer to length in state on timer switch
   useEffect(() => {
     // setTimer(convertToTimer())
-    const current = convertToTimer(millie)
-    if (current === '00:00') {
-      console.log('times up')
-      console.log(current)
-      setSessionTimer(prev => !prev)
+    // const current = convertToTimer(millie)
+    if (millie === 0) {
+      setStart(prev => !prev)
+
       audio.current.play()
+      setTimeout(() => {
+        setSessionTimer(prev => !prev)
+        // audio.current.pause()
+        setStart(prev => !prev)
+        if (intervalId) {
+          clearInterval(intervalId)
+          setIntervalId(0)
+        }
+
+        const newIntervalId = setInterval(() => {
+          setMillie(prev => prev - 1000)
+        }, 1000)
+
+        setIntervalId(newIntervalId)
+      }, 1000)
     }
-
-    // console.log(timer)
-    // timer === '00:00' && console.log(timer)
-    // console.log(audio.current.duration)
-    // console.log(sessionTimer ? 'Session' : 'Break')
   }, [millie])
-
-  // useEffect(() => {
-
-  // }, [millie])
-
-  // listen to start to start and stop countdown
-  // useEffect(() => {
-  //   console.log(start)
-  //   start && interval
-  //   !start && clearInterval(interval)
-
-  // }, [start])
-
-  // const interval = setInterval(countdown, 1000);
-
-  // function countdown() {
-  //   setMillie(prev => prev - 1000)
-  // }
 
   // convert minutes to millieseconds
   function convertToMillie (num) {
     return Math.floor(num * 1000 * 60)
   }
 
-  // use Date to create timer from millie state and return format mm:ss.
-  function convertToTimer (num) {
-    const date = new Date(0)
-    date.setMilliseconds(num)
-    return date.toISOString().substring(14, 19)
-
-    // code below works as well
-    // const minutes = Math.floor(millie / 60000)
-    // const seconds = (millie % 60000) / 1000
-    // return `${minutes
-    //   .toString()
-    //   .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  // convert current milliseconds in to timer of format mm:ss
+  function convertToTimer () {
+    const minutes = Math.floor(millie / 60000)
+    const seconds = (millie % 60000) / 1000
+    return `${minutes
+      .toString()
+      .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
   }
 
   // handle decrement and increment of break and session on click
+  // if intervalId state is true dont run the switch statement
   function handleTimerLength (event) {
     const id = event.currentTarget.id
-    switch (id) {
-      case 'break-decrement':
-        setBreakLength(prev => (prev > 1 ? prev - 1 : prev))
-        break
-      case 'break-increment':
-        setBreakLength(prev => (prev < 60 ? prev + 1 : prev))
-        break
-      case 'session-decrement':
-        setSessionLength(prev => (prev > 1 ? prev - 1 : prev))
-        break
-      case 'session-increment':
-        setSessionLength(prev => (prev < 60 ? prev + 1 : prev))
-        break
-      default:
-        break
+    if (!intervalId) {
+      switch (id) {
+        case 'break-decrement':
+          setBreakLength(prev => (prev > 1 ? prev - 1 : prev))
+          break
+        case 'break-increment':
+          setBreakLength(prev => (prev < 60 ? prev + 1 : prev))
+          break
+        case 'session-decrement':
+          setSessionLength(prev => (prev > 1 ? prev - 1 : prev))
+          break
+        case 'session-increment':
+          setSessionLength(prev => (prev < 60 ? prev + 1 : prev))
+          break
+        default:
+          break
+      }
     }
+    return
   }
-  console.log(sessionLength)
 
   // handle start state with true or false to switch play and start icon
+  // and set or clear countdown interval
   function handleStart () {
     setStart(prev => !prev)
-
+    // console.log('start_stop')
     if (intervalId) {
       clearInterval(intervalId)
       setIntervalId(0)
@@ -121,25 +124,27 @@ const App = () => {
   // handle reset of the app when click reset icon
   function handleReset () {
     intervalId && clearInterval(intervalId)
-    intervalId && setIntervalId(0)
+    setIntervalId(0)
     setBreakLength(5)
     setSessionLength(25)
-    setStart(false)
     setSessionTimer(true)
+    setTimerTitle('Session')
+    setStart(false)
     setMillie(convertToMillie(sessionLength))
     audio.current.pause()
     audio.current.currentTime = 0
   }
 
-  // create play and start icons bases on state
+  // create play and start buttons with icons bases on state
   const play = start ? (
-    <FaPauseCircle id='start_stop' onClick={handleStart} />
+    <button id='start_stop' onClick={handleStart}>
+      <FaPauseCircle />
+    </button>
   ) : (
-    <FaPlayCircle id='start_stop' onClick={handleStart} />
+    <button id='start_stop' onClick={handleStart}>
+      <FaPlayCircle />
+    </button>
   )
-
-  // create the active timer title to be showed
-  const timerTitle = sessionTimer ? 'Session' : 'Break'
 
   return (
     <div id='clock-container'>
@@ -148,30 +153,38 @@ const App = () => {
         <div className='header-controle-box'>
           <h3 id='break-label'>Break Length</h3>
           <div className='inner-controle-box'>
-            <FaArrowAltCircleDown
+            <button
               id='break-decrement'
-              onClick={event => !intervalId && handleTimerLength(event)}
-            />
-            <h3 id='break-length'>{breakLength.toString()}</h3>
-            <FaArrowAltCircleUp
+              onClick={event => handleTimerLength(event)}
+            >
+              <FaArrowAltCircleDown />
+            </button>
+            <h3 id='break-length'>{breakLength}</h3>
+            <button
               id='break-increment'
-              onClick={event => !intervalId && handleTimerLength(event)}
-            />
+              onClick={event => handleTimerLength(event)}
+            >
+              <FaArrowAltCircleUp />
+            </button>
           </div>
         </div>
         <div className='header-controle-box'>
           <h3 id='session-label'>Session Length</h3>
           <div className='inner-controle-box'>
-            <FaArrowAltCircleDown
+            <button
               id='session-decrement'
-              onClick={event => !intervalId && handleTimerLength(event)}
-            />
-
+              onClick={event => handleTimerLength(event)}
+            >
+              <FaArrowAltCircleDown />
+            </button>
             <h3 id='session-length'>{sessionLength}</h3>
-            <FaArrowAltCircleUp
+
+            <button
               id='session-increment'
-              onClick={event => !intervalId && handleTimerLength(event)}
-            />
+              onClick={event => handleTimerLength(event)}
+            >
+              <FaArrowAltCircleUp />
+            </button>
           </div>
         </div>
       </div>
@@ -179,22 +192,24 @@ const App = () => {
         <h3 id='timer-label'>{timerTitle}</h3>
         <div id='timer-controle-container'>
           {play}
-          <div id='time-left'>{convertToTimer(millie)}</div>
-          <IoReloadCircle id='reset' onClick={handleReset} />
+          <div id='time-left'>{convertToTimer()}</div>
+          <button id='reset' onClick={event => handleReset(event)}>
+            <IoReloadCircle />
+          </button>
         </div>
       </div>
       <div id='credentials-box'>
         <p>
           Concept bv{' '}
           <a
-            rel="noreferrer"
+            rel='noreferrer'
             href='https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-25--5-clock'
             target='_blank'
           >
             FCC
           </a>{' '}
           | Build by{' '}
-          <a rel="noreferrer" href='https://github.com/etrigo/' target='_blank'>
+          <a rel='noreferrer' href='https://github.com/etrigo/' target='_blank'>
             Etrigo
           </a>
         </p>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-// import * as ReackDOMClient from 'react-dom/client'
-// import App from './App'
-
-// const container = document.getElementById('root')
-// const root = ReackDOMClient.createRoot(container)
-
-// root.render(<App />)
-
-// teasting...
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReackDOMClient from 'react-dom/client'
 import App from './App'
 
+const container = document.getElementById('root')
+const root = ReackDOMClient.createRoot(container)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+root.render(<App />)
+
+// teasting...
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App'
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
